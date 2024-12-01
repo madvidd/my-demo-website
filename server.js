fc6dc5c0bfc522396e1650bd083119bcdbb39e43
madvidd <69314407+madvidd@users.noreply.github.com>
2024-12-01 20:04:29 +0330

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/dashboard', authenticateJWT, (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
