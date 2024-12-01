406cc410caa966a6c4c593f985c0822def7e15d0
madvidd <69314407+madvidd@users.noreply.github.com>
2024-12-01 21:01:31 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
+// next.config.js
 module.exports = {
-  distDir: 'build', // Optional: specify a custom directory if necessary. Defaults to '.next'
-  // Other configurations can go here as needed.
+  reactStrictMode: true,
 };
